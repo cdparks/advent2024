813c3fe008b34ca08c52d856180b3959c041d68f
Christopher D. Parks <christopher.daniel.parks@gmail.com>
2024-12-15 19:54:23 -0800

    Clean up solution for day 15
    Don't need incremental updates, just grab points for all tiles that can
    move and apply the change all at once

--- src/Advent/Day15Spec.hs
+++ src/Advent/Day15Spec.hs
@@ -7,22 +7,20 @@ module Advent.Day15Spec
 import Advent.Prelude
 
 import Advent.Input
+import Data.HashMap.Strict ((!))
 import Data.HashMap.Strict qualified as HashMap
 import Data.List (head)
 
 spec :: Spec
 spec = do
-  reading (toMapWith pure) 15 $ do
+  reading toMap 15 $ do
     it "1" $ \Input{..} -> do
       uncurry part1 example `shouldBe` 10092
       uncurry part1 problem `shouldBe` 1463715
 
-  reading (toMapWith expand) 15 $ do
     it "2" $ \Input{..} -> do
-      uncurry part2 example `shouldBe` 9021
-      uncurry part2 problem `shouldBe` 1481392
- where
-  expand (x, y) = [(x * 2, y), (x * 2 + 1, y)]
+      uncurry part2 (first expand example) `shouldBe` 9021
+      uncurry part2 (first expand problem) `shouldBe` 1481392
 
 part1 :: HashMap (Int, Int) Tile -> [Heading] -> Int
 part1 = solve False
@@ -31,45 +29,32 @@ part2 :: HashMap (Int, Int) Tile -> [Heading] -> Int
 part2 = solve True
 
 solve :: Bool -> HashMap (Int, Int) Tile -> [Heading] -> Int
-solve wide m0 = gps . go m0 p0
+solve wide m0 =
+  done . go m0 p0
  where
   p0 = head [p | (p, Robot) <- HashMap.toList m0]
   go m p = \case
     [] -> m
     h:hs
-      | Just n <- step m [(q, Robot)] h -> go n q hs
+      | Just ps <- step m p h -> go (update m ps h) (move p h) hs
       | otherwise -> go m p hs
-     where
-      q = move p h
 
-  step m ps h
-    | null ps = Just m
-    | otherwise = case bimap concat concat . unzip <$> traverse (next m h) ps of
-        Nothing -> Nothing
-        Just (qs, dels) -> do
-          -- This is way faster than using Union/Difference, probably for laziness reasons
-          let del = [q | q <- dels, q `notElem` map fst ps]
-          let updated = foldr (uncurry HashMap.insert) (foldr HashMap.delete m del) ps
-          step updated qs h
-
-  next m h (p@(x, y), _tile) =
-    case HashMap.lookup p m of
-      Just Wall ->
-        Nothing
-      Just BoxL
-        | wide && h `elem` [N, S] ->
-          Just ([(move p h, BoxL), (move (x + 1, y) h, BoxR)], [(x + 1, y)])
-        | otherwise ->
-          Just ([(move p h, BoxL)], [])
-      Just BoxR
-        | wide && h `elem` [N, S] ->
-          Just ([(move p h, BoxR), (move (x - 1, y) h, BoxL)], [(x - 1, y)])
-        | otherwise ->
-          Just ([(move p h, BoxR)], [])
-      Just Robot ->
-        Just ([], [])
-      Nothing ->
-        Just ([], [])
+  step m p@(x, y) h = case HashMap.lookup p m of
+    Nothing -> Just []
+    Just Wall -> Nothing
+    Just BoxL
+      | wide && h `elem` [N, S] -> do
+          let q = (x + 1, y)
+          (p :) . (q :) <$> ((<>) <$> next p <*> next q)
+      | otherwise -> (p :) <$> next p
+    Just BoxR
+      | wide && h `elem` [N, S] -> do
+          let q = (x - 1, y)
+          (p:) . (q:) <$> ((<>) <$> next p <*> next q)
+      | otherwise -> (p :) <$> next p
+    Just Robot -> (p :) <$> next p
+   where
+    next q = step m (move q h) h
 
   move (x, y) = \case
     N -> (x, y - 1)
@@ -77,25 +62,41 @@ solve wide m0 = gps . go m0 p0
     W -> (x - 1, y)
     S -> (x, y + 1)
 
-  gps m = sum $ do
+  done m = sum $ do
     ((x, y), BoxL) <- HashMap.toList m
     pure $ x + y * 100
+
+  -- Much faster than union/difference
+  update m ps h =
+    foldr
+      (\q -> HashMap.insert (move q h) (m ! q))
+      (foldr HashMap.delete m ps)
+      ps
 
 data Tile = Wall | BoxL | BoxR | Robot
 
 data Heading = N | E | W | S
   deriving Eq
 
-toMapWith :: ((Int, Int) -> [(Int, Int)]) -> Text -> (HashMap (Int, Int) Tile, [Heading])
-toMapWith f t = first HashMap.fromList $ partitionEithers $ do
+toMap :: Text -> (HashMap (Int, Int) Tile, [Heading])
+toMap t = first HashMap.fromList $ partitionEithers $ do
   (y, line) <- zip [0..] $ lines t
   (x, c) <- zip [0..] $ unpack line
   case c of
-    '#' -> Left . (,Wall) <$> f (x, y)
-    'O' -> Left <$> zip (f (x, y)) [BoxL, BoxR]
-    '@' -> take 1 $ Left . (,Robot) <$> f (x, y)
+    '#' -> pure $ Left ((x, y), Wall)
+    'O' -> pure $ Left ((x, y), BoxL)
+    '@' -> pure $ Left ((x, y), Robot)
     '^' -> pure $ Right N
     '>' -> pure $ Right E
     '<' -> pure $ Right W
     'v' -> pure $ Right S
     _ -> []
+
+expand :: HashMap (Int, Int) Tile -> HashMap (Int, Int) Tile
+expand m = HashMap.fromList $ do
+  ((x, y), t) <- HashMap.toList m
+  case t of
+    Wall -> (,Wall) <$> [(x * 2, y), (x * 2 + 1, y)]
+    BoxL -> [((x * 2, y), BoxL), ((x * 2 + 1, y), BoxR)]
+    BoxR -> []
+    Robot -> [((x * 2, y), Robot)]
